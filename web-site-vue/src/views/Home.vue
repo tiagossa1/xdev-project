c5d1aa5c5b2cddd97e95f01eb104c725e6772d25
John <fernandes.edgarsilva10@gmail.com>
2021-09-28 01:26:54 +0100

    Criacao de modal para criar post.Guardar dados dos posts

--- web-site-vue/src/views/Home.vue
+++ web-site-vue/src/views/Home.vue
@@ -1,11 +1,5 @@
 <template>
   <div class="container">
-    <div class="buttons m-3">
-      <b-button pill variant="info">Criar +</b-button>
-      <b-dropdown class="ml-2" id="dropdown-1" text="Filtro">
-        <b-dropdown-item >First Action</b-dropdown-item>
-      </b-dropdown>
-    </div>
     <post-component></post-component>
   </div>
 </template>
