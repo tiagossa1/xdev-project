cb85120a08cb205064ef60fe0504aba313884b1e
T0119600 <T0119600@ciber.local>
2021-10-18 20:20:11 +0100

    users fixed

--- web-api/database/seeds/UserSeeder.php
+++ web-api/database/seeds/UserSeeder.php
@@ -14,10 +14,10 @@ class UserSeeder extends Seeder
         //factory(\App\User::class,10)->create();
 
         App\User::create([
-            'name' => 'Andre Filipe Marta Teixeira',
+            'name' => 'Andre Teixeira',
             'email' => 'andre.teixeira.t0119600@edu.atec.pt',
             'email_verified_at' => now(),
-            'password' => bcrypt('AndreAdmin'),
+            'password' => bcrypt('password'),
             'birth_date' => '1992-11-18',
             'github_url' => "www.github.com",
             'linkedin_url' => "www.linked.com",
@@ -31,26 +31,26 @@ class UserSeeder extends Seeder
 
         App\User::create([
             'name' => 'Tiago Sá',
-            'email' => 'tiago.sa.t0119600@edu.atec.pt',
+            'email' => 'tiago.sa.t0119181@edu.atec.pt',
             'email_verified_at' => now(),
-            'password' => bcrypt('TiagoAdmin'),
-            'birth_date' => '1992-11-18',
+            'password' => bcrypt('password'),
+            'birth_date' => '2000-10-30',
             'github_url' => "www.github.com",
             'linkedin_url' => "www.linked.com",
             'facebook_url' => "www.facebook.com",
             'instagram_url' => "www.instagram.com",
             'user_type_id' => 4,
-            'district_id' => 13,
+            'district_id' => 3,
             'school_class_id' => 51,
             'suspended' => 0
         ]);
 
         App\User::create([
             'name' => 'Jorge Faria',
-            'email' => 'jorge.faria.t0119600@edu.atec.pt',
+            'email' => 'jorge.faria.t0119174@edu.atec.pt',
             'email_verified_at' => now(),
-            'password' => bcrypt('JorgeAdmin'),
-            'birth_date' => '1992-11-18',
+            'password' => bcrypt('password'),
+            'birth_date' => '1991-08-08',
             'github_url' => "www.github.com",
             'linkedin_url' => "www.linked.com",
             'facebook_url' => "www.facebook.com",
@@ -63,10 +63,10 @@ class UserSeeder extends Seeder
 
         App\User::create([
             'name' => 'Edgar Fernandes',
-            'email' => 'edgar.fernandes.t0119600@edu.atec.pt',
+            'email' => 'edgar.fernandes.t0119166@edu.atec.pt',
             'email_verified_at' => now(),
-            'password' => bcrypt('EdgarAdmin'),
-            'birth_date' => '1992-11-18',
+            'password' => bcrypt('password'),
+            'birth_date' => '1999-05-31',
             'github_url' => "www.github.com",
             'linkedin_url' => "www.linked.com",
             'facebook_url' => "www.facebook.com",
