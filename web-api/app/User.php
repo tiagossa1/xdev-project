70577979cf5bdeeea9e4942457a97527810f792d
pintarolas <78449233+pintarolas@users.noreply.github.com>
2021-10-20 21:01:44 +0100

    User implemented MustVerifyEmail

--- web-api/app/User.php
+++ web-api/app/User.php
@@ -2,15 +2,15 @@
 
 namespace App;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasApiTokens, Notifiable;
-    
+
     protected $with = ["district", 'school_class', "user_type", 'tags'];
 
     protected $fillable = [
