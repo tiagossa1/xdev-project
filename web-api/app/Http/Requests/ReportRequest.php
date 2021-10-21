a969a299f4d28559fb1f40c7556bb4ce6373b3b4
jorgefaria <jorgefaria8891@gmail.com>
2021-10-21 20:39:34 +0100

    Report post_id & comment_id fixed

--- web-api/app/Http/Requests/ReportRequest.php
+++ web-api/app/Http/Requests/ReportRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\OneOf;
 
 class ReportRequest extends FormRequest
 {
@@ -15,10 +16,18 @@ class ReportRequest extends FormRequest
     {
         return [
             'user_id' => 'required',
-            'post_id' => 'required_without:comment_id',
-            'comment_id' => 'required_without:post_id',
+            //'post_id' => 'required_without:comment_id',
+            //'comment_id' => 'required_without:post_id',
             'closed' => 'required|boolean',
             'reason' => 'required',
+            'post_id' => [
+                'bail',
+                function ($attribute, $value, $fail) {
+                    if (request()->has($attribute) === request()->filled('comment_id')) {
+                        return $fail('Only 1 of the two is allowed');
+                    }
+                }],
+            'comment_id' => ['required_without:post_id','nullable'],
         ];
     }
 }
