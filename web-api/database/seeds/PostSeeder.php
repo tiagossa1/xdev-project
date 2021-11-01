1a51c6ea14e3c9a5c260a010233452ba3249a5fa
tiagossa1 <tssa.2000@tuta.io>
2021-11-01 18:26:24 +0000

    + Fixed post seeder giving error due to same values.

--- web-api/database/seeds/PostSeeder.php
+++ web-api/database/seeds/PostSeeder.php
@@ -12,27 +12,31 @@ class PostSeeder extends Seeder
      */
     public function run()
     {
+        // $numbers = range(1, 28);
+        // shuffle($numbers);
+        $faker = Faker\Factory::create();
+
         factory('App\Post', 10)->create();
 
         for ($i=1; $i <= 10; $i++) {
             DB::table('post_tag')->insert(
                 [
                     'post_id' => $i,
-                    'tag_id' => rand(1, 28),
+                    'tag_id' => $faker->unique(true)->numberBetween(1, 28),
                 ]
             );
 
             DB::table('post_tag')->insert(
                 [
                     'post_id' => $i,
-                    'tag_id' => rand(1, 28),
+                    'tag_id' => $faker->unique(true)->numberBetween(1, 28),
                 ]
             );
 
             DB::table('post_tag')->insert(
                 [
                     'post_id' => $i,
-                    'tag_id' => rand(1, 28),
+                    'tag_id' => $faker->unique(true)->numberBetween(1, 28),
                 ]
             );
         }
